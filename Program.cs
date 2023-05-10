7e1aee13fd0ba81b9e6e9688bd21fa937f345eff
Ambatukammm <PRASR7@student.op.ac.nz>
2023-05-11 10:30:34 +1200

    added a game

--- Program.cs
+++ Program.cs
@@ -6,9 +6,45 @@ namespace Practise_Git
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi im the asian");
-            Console.WriteLine("Hello im Harry");
-            Console.WriteLine("Halo I'm Dumb");
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+            int attempts = 0;
+            bool isCorrect = false;
+
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+
+            while (!isCorrect)
+            {
+                Console.Write("Enter your guess (1-100): ");
+                string input = Console.ReadLine();
+                int guess;
+
+                if (int.TryParse(input, out guess))
+                {
+                    attempts++;
+
+                    if (guess < secretNumber)
+                    {
+                        Console.WriteLine("Too low! Try again.");
+                    }
+                    else if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Too high! Try again.");
+                    }
+                    else
+                    {
+                        isCorrect = true;
+                        Console.WriteLine($"Congratulations! You guessed the secret number {secretNumber} in {attempts} attempts.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
